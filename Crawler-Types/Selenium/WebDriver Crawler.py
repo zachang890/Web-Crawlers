96095f33da7183e3a37b599cd464f808ad6e83d6
zachang890 <zachang890@gmail.com>
2020-08-23 15:50:20 -0700

    Selenium xpath

--- Crawler-Types/Selenium/WebDriver Crawler.py
+++ Crawler-Types/Selenium/WebDriver Crawler.py
@@ -8,16 +8,18 @@ chrome_options.add_argument("--window-size=1920x1080")
 
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="./chromedriver")
 
-url = "https://news.ycombinator.com/"
+url = "https://artofproblemsolving.com/"
 driver.get(url)
 time.sleep(2)
 
 #unicode_article = driver.find_element_by_id('24196025')
-elements = driver.find_elements_by_css_selector(".storylink")
-storyTitles = [el.text for el in elements]
-scores = driver.find_elements_by_css_selector(".score")
-storyScores = [score.text for score in scores]
-links = [el.get_attribute("href") for el in elements]
-print(storyTitles)
-print(links)
-print(storyScores)
+# elements = driver.find_elements_by_css_selector(".storylink")
+# storyTitles = [el.text for el in elements]
+# scores = driver.find_elements_by_css_selector(".score")
+# storyScores = [score.text for score in scores]
+# links = [el.get_attribute("href") for el in elements]
+# print(storyTitles)
+# print(links)
+# print(storyScores)
+toptitle = driver.find_element_by_xpath("/html/body").get_attribute("class")
+print(toptitle)
